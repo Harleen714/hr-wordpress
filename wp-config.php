a832bf0344f7e65cdaf2ee2b78d6176889f2730b
Kamal <93322430+kamalsq@users.noreply.github.com>
2021-11-23 15:18:23 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -96,3 +96,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+define('WP_HOME','https://kamal.squareops.co.in');
+define('WP_SITEURL','https://kamal.squareops.co.in');
+if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)  $_SERVER['HTTPS']='on';
